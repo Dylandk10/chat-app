9a68ae671dc9502b992c32f0c71c60e31fbe3681
Dylan Kelly <dylankelly@Dylans-MBP.fios-router.home>
2017-12-19 12:01:29 -0500

    Added connect and disconnect handlers

--- server/server.js
+++ server/server.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const path = require('path');
+const http = require('http');
+const socketIO = require('socket.io');
 const publicPath = path.join(__dirname, '../public');
-var app = express();
 const port = process.env.PORT || 3000;
+
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+  console.log("New User Connected");
+
+  socket.on('disconnect', () => {
+    console.log("User was disconnected");
+  });
+});
+
+
+
+
+server.listen(port, () => {
   console.log(`Server running on port: ${port}`);
 });
