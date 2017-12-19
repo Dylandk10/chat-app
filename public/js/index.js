517230a657b4470507144af8ec65ba5c8c21784e
Dylan Kelly <dylankelly@Dylans-MBP.fios-router.home>
2017-12-19 18:02:02 -0500

    Added server for display all message

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function() {
   console.log("Connected to server...");
-
-  socket.emit('createMessage', {
-    from: 'Jen@example.com',
-    text: 'test test test test test test'
-  });
 });
 
 socket.on('disconnect', function() {
