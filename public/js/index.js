e75fe0ea2a2d53dd6a65dab982ea9e5537e229b2
Dylan Kelly <dylankelly@Dylans-MBP.fios-router.home>
2017-12-20 15:09:36 -0500

    test commit

--- public/js/index.js
+++ public/js/index.js
@@ -11,3 +11,21 @@ socket.on('disconnect', function() {
 socket.on('newMessage', function(message) {
   console.log("New message ", message);
 });
+
+socket.emit('createMessage', {
+  from: 'fank',
+  text: "heyyyyy"
+}, function(data) {
+  console.log("Got it", data);
+});
+
+jQuery('#message-form').on('submit', function(e) {
+  e.preventDefault();
+
+  socket.emit('createMessage', {
+    from: 'User',
+    text: jQuery('[name=message]').val()
+  }, function() {
+
+  });
+});
--- server/server.js
+++ server/server.js
@@ -19,14 +19,10 @@ io.on('connection', (socket) => {
   socket.emit('newMessage', generateMessage('Admin', 'Welcome To chat App'));
   socket.broadcast.emit('newMessage', generateMessage('Admin', 'New User Joined'));
 
-  socket.on('createMessage', (message) => {
+  socket.on('createMessage', (message, callback) => {
     console.log('Created Message: ', message);
     io.emit('newMessage', generateMessage(message.from, message.text));
-    // socket.broadcast.emit('newMessage', {
-    //   from: message.from,
-    //   text: message.text,
-    //   createdAt: new Date().getTime()
-    // });
+    callback("This is from the server");
   });
 
   socket.on('disconnect', () => {
