0dbebf13b24079a7ac0a7a00fea0dd3957f40994
Dylan Kelly <dylankelly@Dylans-MBP.fios-router.home>
2017-12-21 11:45:00 -0500

    Made location button and render to DOM

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const http = require('http');
 const socketIO = require('socket.io');
 const publicPath = path.join(__dirname, '../public');
 //message file...
-const {generateMessage} = require('./utils/message');
+const {generateMessage, generateLocationMessage} = require('./utils/message');
 const port = process.env.PORT || 3000;
 
 var app = express();
@@ -23,6 +23,10 @@ io.on('connection', (socket) => {
     console.log('Created Message: ', message);
     io.emit('newMessage', generateMessage(message.from, message.text));
     callback("This is from the server");
+  });
+
+  socket.on('createLocationMessage', (coords) => {
+    io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
   });
 
   socket.on('disconnect', () => {
