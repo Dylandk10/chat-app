c9d0f20ff48391bc7e3181bc562e07809e13323b
Dylan Kelly <dylankelly@Dylans-MBP.fios-router.home>
2018-01-03 11:38:24 -0500

    Formatted time stamps using moment npm

---
+++ playground/time.js
@@ -0,0 +1,8 @@
+var moment = require('moment');
+
+var date = moment();
+//testing method...
+//date.add(1, 'years');
+
+console.log(date.format('MMM Do, YYYY'));
+console.log(date.format('h:mm a'));
